d0469cdedf8ed2c533a310e189125030d1b71c49
CaoXueLiang <2357747972@qq.com>
2022-11-18 15:37:51 +0800

    备份

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import './assets/main.css'
+import './assets/main.css';
 
-createApp(App).mount('#app')
+import { sonAMethod, sonBMethod } from 'component_repo';
+
+console.log(sonAMethod('lining', 'hi'));
+console.log(sonBMethod(10, 20));
+
+createApp(App).mount('#app');
